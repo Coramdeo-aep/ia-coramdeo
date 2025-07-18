1a9af818cdf43f039a91a63839731151da1507e6
Coramdeo-aep <coramdeo.aep@gmail.com>
2025-07-18 16:33:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # URL do webhook do n8n
-N8N_WEBHOOK_URL = "https://n8n.diferro.com.br:5678/webhook/chat"
+N8N_WEBHOOK_URL = "https://n8n.diferro.com.br:5678/webhook-test/chat"
 
 st.title("Chat com n8n")
 
